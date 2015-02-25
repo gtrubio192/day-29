67938c434a2e6f67694d810a30b6fa94e8fd5d3c
Gabe Rubio <gtrubio192@gmail.com>
2015-02-24 18:10:37 -0600

    Filter fully working for title and quantity. Up and down arrows toggling when clicked

--- Etsy/scripts/controllers.js
+++ Etsy/scripts/controllers.js
@@ -4,11 +4,15 @@ angular.module('app.controllers', [])
     $scope.listingsCopy = [];
     $scope.filter= "";
     $scope.alpha = false;
+    $scope.num = false;
+    $scope.titleArrow = false;
+    $scope.quantArrow = false;
+    
     $http.get('https://openapi.etsy.com/v2/listings/active?api_key=vx1kjbu94swwwu89amphy9ww')
         .success(function(response) {
             $scope.listings = response.results;
             $scope.listingsCopy = response.results;
-            console.log($scope.listings);
+//            console.log($scope.listings);
             
         })
         .error(function(response, status, headers, config){
@@ -19,24 +23,53 @@ angular.module('app.controllers', [])
     $scope.reverseState = function(){
         if(!$scope.alpha)
         {
+            $scope.listingsCopy = _.sortBy($scope.listingsCopy,function(element){
+                return element.title.toLowerCase();
+            });
             $scope.alpha = true;
-            $scope.listings
         }
-        $scope.listings.reverse();    
+        else{
+            $scope.listingsCopy.reverse();    
+        }
+        $scope.titleArrow = !$scope.titleArrow;
     };
     
-    $scope.
+    $scope.reverseQuantity = function(){
+        if(!$scope.num)
+        {
+            $scope.listingsCopy = _.sortBy($scope.listingsCopy,function(element){
+                console.log($scope.listingsCopy);
+                return element.quantity;
+            });
+            $scope.num = true;
+        }
+        else{
+            $scope.listingsCopy.reverse();    
+        }
+        $scope.quantArrow = !$scope.quantArrow;
+    };
+    
     
     $scope.$watch('filter', 
          function() {
+            'use strict';
             console.log('filter: ' + $scope.filter);
             $scope.listingsCopy = _.filter($scope.listings,
                 function(element){
-                    console.log('filter inside: ' + $scope.filter);
-                    return 
-                        element.title.toLowerCase().indexOf($scope.filter.toLowerCase()) > -1;
+//                element.title.toLowerCase().indexOf($scope.filter.toLowerCase())
+                    var quantity = element.quantity;                
+                    var title = element.title.toLowerCase();
+                    var quantFilter = $scope.filter;
+                
+            
+                    var filter = $scope.filter.toLowerCase();
+                    var titleFilter = title.indexOf(filter) > -1;
+//                    var quantityFilter = quantity.indexOf(filter) > -1;
+
+                    return  titleFilter;
+                });
+//        console.log($scope.listingsCopy);
         });
-    });
 
 });
     
